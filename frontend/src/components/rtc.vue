6a9e21194fade860ddb4b3e2cbf0439685d1c689
gusheng <605661239@qq.com>
2018-07-29 23:11:44 +0800

    update

--- frontend/src/components/rtc.vue
+++ frontend/src/components/rtc.vue
@@ -141,15 +141,15 @@ export default {
       }
       window.file = file
       rtcManager.emit('chat-file', info.hash, file)
-      // console.time('slice')
-      // let chunk = 1024 * 50 // 最大为64*1024
-      // for (let start = 0; start < file.size; start += chunk) {
-      //   const end = Math.min(start + chunk, file.size)
-      //   const perfile = file.slice(start, end)
-      //   rtcManager.emit('chat-file', info.hash, perfile)
-      //   // await new Promise(setTimeout)
-      // }
-      // console.timeEnd('slice')
+        // console.time('slice')
+        // let chunk = 1024 * 50 // 最大为64*1024
+        // for (let start = 0; start < file.size; start += chunk) {
+        //   const end = Math.min(start + chunk, file.size)
+        //   const perfile = file.slice(start, end)
+        //   rtcManager.emit('chat-file', info.hash, perfile)
+        //   // await new Promise(setTimeout)
+        // }
+        // console.timeEnd('slice') 
     },
     async sendFiles(files) {
       files.forEach(it => {

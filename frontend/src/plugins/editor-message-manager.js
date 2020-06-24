25bf124b6902dcfa63e8d63b10462e127af995bd
gusheng <605661239@qq.com>
2020-06-24 18:42:41 +0800

    1

---
+++ frontend/src/plugins/editor-message-manager.js
@@ -0,0 +1,93 @@
+import { fileReader } from '@/tools'
+
+export default class {
+  videos = []
+  files = [] //所有结果
+  autofiles = [] // 非video，img,的文件
+  async addImg(file, area) {
+    const dataurl = await fileReader({ data: file })
+    const img = `<img class="chat-img"  src="${dataurl}" >`
+    area.innerHTML += img
+  }
+  async addVideo(file, area) {
+    file = await fileReader({ data: file, type: 'blob' })
+    const url = URL.createObjectURL(file)
+    const hash = (Math.random() * 10 ** 17).toString()
+    let html = `<video  hash="${hash}"  controls class="chat-video" src="${url}"></video>`
+    this.videos.push({
+      hash,
+      file
+    })
+    area.innerHTML += html
+  }
+
+  async addFile(file, area) {
+    const name = file.name
+    const hash = (Math.random() * 10 ** 17).toString()
+    console.time('111')
+    console.log(file)
+    // file = await fileReader({ data: file, type: 'blob' })
+    console.timeEnd('111')
+    let html = `<div hash="${hash}"><i class="v-icon-document file-icon"></i><span class="file-name"> ${name}</span></div>`
+    this.autofiles.push({
+      file,
+      hash,
+      name
+    })
+    area.innerHTML += html
+  }
+
+  filterVideo(area) {
+    let index = 0
+    this.videos.forEach(it => {
+      const video = area.querySelector(`[hash="${it.hash}"]`)
+
+      if (!video) return
+      area.removeChild(video)
+
+      this.files.push({
+        type: 'video',
+        file: it.file,
+        hash: it.hash,
+        index: index++
+      })
+    })
+  }
+
+  filterfile(area) {
+    let index = 0
+    this.autofiles.forEach(it => {
+      const file = area.querySelector(`[hash="${it.hash}"]`)
+      if (!file) return
+      const fileName = file.querySelector('.file-name').innerHTML
+      area.removeChild(file)
+
+      this.files.push({
+        type: 'file',
+        file: it.file,
+        fileName,
+        hash: it.hash,
+        index: index++
+      })
+    })
+  }
+
+  filter(area) {
+    area = area.cloneNode(true)
+    this.filterVideo(area)
+    this.filterfile(area)
+    const text = area.innerHTML
+    const files = [...this.files]
+    this.clear()
+    return {
+      text,
+      files
+    }
+  }
+
+  clear() {
+    this.autofiles = []
+    this.files = []
+    this.videos = []
+  }
+}
